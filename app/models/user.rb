8032bdc2551ffd4d840fd8ac04229ec52f679ae1
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-03-09 06:58:25 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "英数字を両方使用してください"}
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]\z/, message: "全角文字(漢字、ひらがな、カタカナ)のみ使えます"}
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]\z/, message: "全角文字(漢字、ひらがな、カタカナ)のみ使えます"}
-  validates :katakana_first_name, presence: true, format: {with: /\A[ァ-ン一]\z/, message: "全角カタカナのみ使えます"}
-  validates :katakana_last_name, presence: true, format: {with: /\A[ァ-ン一]\z/, message: "全角カタカナのみ使えます"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶー－龥]+\z/, message: "全角文字(漢字、ひらがな、カタカナ)のみ使えます"}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶー－龥]+\z/, message: "全角文字(漢字、ひらがな、カタカナ)のみ使えます"}
+  validates :katakana_first_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみ使えます"}
+  validates :katakana_last_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみ使えます"}
   validates :birth_day, presence: true
 
 
