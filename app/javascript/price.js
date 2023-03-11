83a150ced6e7e3985dc2d4e9f684868ca8ab6541
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-03-11 18:08:57 +0900

    価格に小数点以下の値が入った際の挙動修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,8 +5,8 @@ function price() {
   priceForm.addEventListener('keyup', () => {
     const taxElement = document.getElementById('add-tax-price');
     const profitElement = document.getElementById('profit');
-    const price = priceForm.value;
-    const tax = Math.round(price * 0.1);
+    const price = Math.floor(priceForm.value);
+    const tax = Math.floor(price * 0.1);
     const profit = price - tax;
     taxElement.innerHTML = tax;
     profitElement.innerHTML = profit;
