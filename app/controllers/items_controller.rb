26f768b9f78ad760c30054effedc937c77ce8c53
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-03-16 21:41:29 +0900

    rubocopによる校正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user_id == current_user.id
-      redirect_to root_path
-    end
+    return if @item.user_id == current_user.id
+
+    redirect_to root_path
   end
 
   def update
